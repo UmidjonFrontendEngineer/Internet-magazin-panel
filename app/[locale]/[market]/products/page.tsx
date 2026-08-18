5cc6566348bf9f162574cfe9ce7916ccb02579d7
umidjon sharipov <umidjonsharipov283@gmail.com>
2026-08-18 21:42:27 +0500

    updated

--- app/[locale]/[market]/products/page.tsx
+++ app/[locale]/[market]/products/page.tsx
@@ -558,14 +558,31 @@ const ProductsGet = () => {
                 <form onSubmit={handleCreateProduct}>
 
                     <div className="w-full flex gap-4 max-[650px]:flex-col">
-                        <div className="w-full p-1 flex gap-2 flex-col">
+                        <div className="w-full p-1 flex gap-2">
+                            <div className="flex flex-col gap-3">
+                                {Array.from({ length: imagesLength }).map((_, index) => (
+                                    <Item key={index} cIndex={index} setItemsLenght={setItemsLenght} />
+                                ))}
 
+                                <GlassButton>
+                                    +
+                                </GlassButton>
+                            </div>
+
+                            <div className="flex flex-col gap-3">
+                                <GlassInput label='price' name='price' placeholder="price..." />
+                                <GlassButton>category select</GlassButton>
+                                <GlassButton>warehouse select</GlassButton>
+                                <GlassButton>gradient</GlassButton>
+                            </div>
                         </div>
                         <div className="w-full p-1 flex gap-2 flex-col">
+                            <GlassInput placeholder='title' name='title' label='title' />
+
                             <div>
                                 <div className="flex w-full">
                                     {lans.map(item => (
-                                        <button key={item} type="button" className={`relative px-4 py-2 text-sm focus:border-sky-500/60 focus:shadow-[0_0_0_3px_rgba(14,165,233,0.15)] ${descr === item ? `bg-sky-200/10 border-t border-l border-r ${dark
+                                        <button key={item} type="button" className={`relative px-4 py-2 duration-200 text-sm ${item === descr ? 'border-sky-500/60 shadow-[0_0_0_3px_rgba(14,165,233,0.15)]' : ''} ${descr === item ? `bg-sky-200/10 border-t border-l border-r ${dark
                                             ? 'bg-white/5 border-white/10 text-white placeholder:text-neutral-500'
                                             : 'bg-white/60 border-sky-200/60 text-neutral-900 placeholder:text-neutral-400'}` : ''} translate-y-[2px] z-[10] rounded-xl rounded-bl-[0] rounded-br-[0]`} onClick={() => setDescr(item)}>
                                             {item === 'uz' ? 'O\'zbek' : item === 'en' ? 'English' : item === 'ru' ? 'Русский' : ''}
@@ -573,15 +590,15 @@ const ProductsGet = () => {
                                     ))}
                                 </div>
                                 <div className="flex gap-4">
-                                    <textarea name='descriptionUz' className={`${descr === 'uz' ? '' : 'hidden'} rounded-[1rem] focus:border-sky-500/60 focus:shadow-[0_0_0_3px_rgba(14,165,233,0.15)] rounded-tl-[0] bg-sky-200/10 border py-2 px-4 w-full outline-none text-sm ${dark
+                                    <textarea name='descriptionUz' className={`${descr === 'uz' ? '' : 'hidden'} rounded-[1rem] duration-200 focus:border-sky-500/60 focus:shadow-[0_0_0_3px_rgba(14,165,233,0.15)] rounded-tl-[0] bg-sky-200/10 border py-2 px-4 w-full outline-none text-sm ${dark
                                         ? 'bg-white/5 border-white/10 text-white placeholder:text-neutral-500'
                                         : 'bg-white/60 border-sky-200/60 text-neutral-900 placeholder:text-neutral-400'
                                         }`} placeholder="UZ description..." rows={6}></textarea>
-                                    <textarea name='descriptionEn' className={`${descr === 'en' ? '' : 'hidden'} rounded-[1rem] focus:border-sky-500/60 focus:shadow-[0_0_0_3px_rgba(14,165,233,0.15)] rounded-tl-[0] bg-sky-200/10 border py-2 px-4 w-full outline-none text-sm ${dark
+                                    <textarea name='descriptionEn' className={`${descr === 'en' ? '' : 'hidden'} rounded-[1rem] duration-200 focus:border-sky-500/60 focus:shadow-[0_0_0_3px_rgba(14,165,233,0.15)] rounded-tl-[0] bg-sky-200/10 border py-2 px-4 w-full outline-none text-sm ${dark
                                         ? 'bg-white/5 border-white/10 text-white placeholder:text-neutral-500'
                                         : 'bg-white/60 border-sky-200/60 text-neutral-900 placeholder:text-neutral-400'
                                         }`} placeholder="EN description..." rows={6}></textarea>
-                                    <textarea name='descriptionRu' className={`${descr === 'ru' ? '' : 'hidden'} rounded-[1rem] focus:border-sky-500/60 focus:shadow-[0_0_0_3px_rgba(14,165,233,0.15)] rounded-tl-[0] bg-sky-200/10 border py-2 px-4 w-full outline-none text-sm ${dark
+                                    <textarea name='descriptionRu' className={`${descr === 'ru' ? '' : 'hidden'} rounded-[1rem] duration-200 focus:border-sky-500/60 focus:shadow-[0_0_0_3px_rgba(14,165,233,0.15)] rounded-tl-[0] bg-sky-200/10 border py-2 px-4 w-full outline-none text-sm ${dark
                                         ? 'bg-white/5 border-white/10 text-white placeholder:text-neutral-500'
                                         : 'bg-white/60 border-sky-200/60 text-neutral-900 placeholder:text-neutral-400'
                                         }`} placeholder="RU description..." rows={6}></textarea>
